9e4af9fdfe87082802788d389eb2c8c7cd8903d3
limburamu <limburamu23@gmail.com>
2022-01-11 12:39:54 +0545

    created todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,14 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'todo';
+// }                                                        previous work
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +18,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
-}
+  list: any[] = [];
+
+  addTask(item: string) {
+    this.list.push({id:this.list.length,name:item});
+    console.log(this.list)
+  }
+
+  removeTask(id: number) {
+    console.log(id)
+    this.list=this.list.filter(item=>item.id!== id);
+  }
+}      
+
